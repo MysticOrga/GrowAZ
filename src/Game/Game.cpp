8b4e61538b73ead77c51e561d385eb2703e4801c
LoicR <loic.rabearivelo@epitech.eu>
2025-11-29 01:08:06 +0400

    feat(game): game logic.

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -42,25 +42,25 @@ void Game::handleBuffing(const Object obj)
 {
     switch (obj.getStatToBuff())
     {
-        case StatBuff::LEAF_DROP:
-            _tree._leafDropRate += obj.getBuff();
-            std::cout << "Leaf drop: " << _tree._leafDropRate << std::endl;
-            break;
-        case StatBuff::TREE_SIZE:
-            _tree._height += obj.getBuff();
-            std::cout << "tree size: " << _tree._height << std::endl;
-            break;
-        case StatBuff::CLIENT_DROP:
-            clientRate += obj.getBuff();
-            break;
-        case StatBuff::MALUS_DROP:
-            malusRate += obj.getBuff();
-            break;
-        case StatBuff::COPS_DROP:
-            policeRate += obj.getBuff();
-            break;
-        default:
-            break;
+    case StatBuff::LEAF_DROP:
+        _tree._leafDropRate += obj.getBuff();
+        std::cout << "Leaf drop: " << _tree._leafDropRate << std::endl;
+        break;
+    case StatBuff::TREE_SIZE:
+        _tree._height += obj.getBuff();
+        std::cout << "tree size: " << _tree._height << std::endl;
+        break;
+    case StatBuff::CLIENT_DROP:
+        clientRate += obj.getBuff();
+        break;
+    case StatBuff::MALUS_DROP:
+        malusRate += obj.getBuff();
+        break;
+    case StatBuff::COPS_DROP:
+        policeRate += obj.getBuff();
+        break;
+    default:
+        break;
     }
 }
 
@@ -68,7 +68,8 @@ void Game::handleEvents()
 {
     Vector2 mousePos = _raylib->getMousePosition();
 
-    if (CheckCollisionPointRec(mousePos, _clickArea) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) && _cycleType == DAY)
+    if (CheckCollisionPointRec(mousePos, _clickArea) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+        _cycleType == DAY)
     {
         double randomChance = (double)rand() / RAND_MAX;
         _cycleTimer += 2.0f;
@@ -84,7 +85,8 @@ void Game::handleEvents()
         _clickCount++;
     }
 
-    if (CheckCollisionPointRec(mousePos, _shopArea) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) && _cycleType == DUSK)
+    if (CheckCollisionPointRec(mousePos, _shopArea) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+        _cycleType == DUSK)
     {
         std::cout << "Clicked inside the shop area!" << std::endl;
     }
@@ -94,9 +96,12 @@ void Game::handleEvents()
     }
 
     int yPos = 100;
-    for (const auto& itemPair : _shop.getObjectList()) {
+    for (const auto &itemPair : _shop.getObjectList())
+    {
         Rectangle itemButton = {_shopArea.x + 20, (float)yPos, _shopArea.width - 40, 50};
-        if (CheckCollisionPointRec(mousePos, itemButton) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) && _cycleType == DUSK) {
+        if (CheckCollisionPointRec(mousePos, itemButton) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+            _cycleType == DUSK)
+        {
             Object boughtObject;
             std::cout << "Trying to buy: " << itemPair.first << std::endl;
             if (_shop.buyObject(itemPair.first, boughtObject, money))
@@ -106,8 +111,11 @@ void Game::handleEvents()
     }
 
     Rectangle sellButton = {_statArea.x + _statArea.width - 220, _statArea.height - 70, 200, 50};
-    if (CheckCollisionPointRec(mousePos, sellButton) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) && _cycleType == DUSK) {
-        if (_leafs > 0) {
+    if (CheckCollisionPointRec(mousePos, sellButton) && _raylib->isMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+        _cycleType == DUSK)
+    {
+        if (_leafs > 0)
+        {
             _leafs--;
             money += 100;
             std::cout << "Vendu 1 feuille pour 100$. Argent actuel: " << money << std::endl;
@@ -205,12 +213,14 @@ void Game::drawShop()
 {
     int yPos = 100;
     int fontSize = 20;
-    for (const auto& itemPair : _shop.getObjectList()) {
+    for (const auto &itemPair : _shop.getObjectList())
+    {
         Rectangle itemButton = {_shopArea.x + 20, (float)yPos, _shopArea.width - 40, 50};
         _raylib->drawRectangleRec(itemButton, LIGHTGRAY);
         _raylib->drawText(itemPair.first.c_str(), itemButton.x + 10, itemButton.y + 15, fontSize, BLACK);
         std::string priceText = std::to_string(itemPair.second) + " $";
-        _raylib->drawText(priceText, itemButton.x + itemButton.width - _raylib->measureText(priceText, fontSize) - 10, itemButton.y + 15, fontSize, DARKGREEN);
+        _raylib->drawText(priceText, itemButton.x + itemButton.width - _raylib->measureText(priceText, fontSize) - 10,
+                          itemButton.y + 15, fontSize, DARKGREEN);
         yPos += 60;
     }
 }
