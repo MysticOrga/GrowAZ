6ba547fdf96c05dbb5492f3c9b18c877c03c8713
LoicR <loic.rabearivelo@epitech.eu>
2025-11-28 21:01:13 +0400

    feat(Cmake): Init Cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-/*
-** EPITECH PROJECT, 2025
-** GrosAZ
-** File description:
-** main
-*/
+#include "raylib.h"
 
+int main(void)
+{
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+
+    InitWindow(screenWidth, screenHeight, "Ma première fenêtre Raylib");
+    SetTargetFPS(90);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Bravo ! Vous avez initialisé votre projet Raylib !", 110, 280, 20, DARKGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
